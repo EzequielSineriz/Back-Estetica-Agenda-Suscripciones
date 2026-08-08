871e90b51fea3821a0d760fdcb931c541dc8859b
EzequielSineriz <ezequielsineriz@gmail.com>
2026-08-08 10:14:01 -0300

    arreglo service

--- src/main/java/com/AppEstetica/controller/auth/AuthController.java
+++ src/main/java/com/AppEstetica/controller/auth/AuthController.java
@@ -4,6 +4,7 @@ import com.AppEstetica.dto.request.AuthRequest;
 import com.AppEstetica.dto.request.RegisterRequest;
 import com.AppEstetica.dto.response.TokenResponse;
 import com.AppEstetica.service.Security.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class AuthController {
     private final AuthService service;
 
     @PostMapping("/register")
-    public ResponseEntity<TokenResponse> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<TokenResponse> register(@Valid @RequestBody RegisterRequest request) {
         final TokenResponse response = service.register(request);
         return ResponseEntity.ok(response);
     }
