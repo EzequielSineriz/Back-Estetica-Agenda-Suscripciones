52095009fb82644f966197f6df602955dea73ea9
EzequielSineriz <ezequielsineriz@gmail.com>
2026-08-08 16:18:33 -0300

    cambios

--- src/main/java/com/AppEstetica/repository/ModuloCursoRepository.java
+++ src/main/java/com/AppEstetica/repository/ModuloCursoRepository.java
@@ -2,9 +2,11 @@ package com.AppEstetica.repository;
 
 import com.AppEstetica.entities.ModuloCurso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ModuloCursoRepository extends JpaRepository<ModuloCurso,Long> {
 
     List<ModuloCurso> findByCursoIdOrderByOrdenAsc(Long cursoId);
